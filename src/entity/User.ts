6915e0c52a43f3f87257acff4865db08b707d2ae
Ray Romero <chinoromero213@gmail.com>
2021-02-20 16:33:00 -0800

    set up for register

---
+++ src/entity/User.ts
@@ -0,0 +1,57 @@
+import { IsEmail, Length } from 'class-validator';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  Index,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BeforeInsert,
+} from 'typeorm';
+
+import bcrypt from 'bcrypt';
+import { classToPlain, Exclude } from 'class-transformer';
+
+@Entity('users')
+export class User extends BaseEntity {
+  constructor(user: Partial<User>) {
+    super();
+    Object.assign(this, user);
+  }
+
+  @Exclude()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Index()
+  // Custom message errors
+  @Length(3, 255, { message: 'Username must be at least 3 characters long' })
+  @Column({ unique: true })
+  username: string;
+
+  @Index()
+  @IsEmail()
+  @Column({ unique: true })
+  email: string;
+
+  @Exclude()
+  @Column()
+  @Length(6, 255)
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
+
+  toJSON() {
+    return classToPlain(this);
+  }
+}
