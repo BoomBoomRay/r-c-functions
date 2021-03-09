164ec909f687f857c938e8bae51802b45362109a
Ray Romero <chinoromero213@gmail.com>
2021-03-08 21:03:48 -0800

    login/register client complete

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,13 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(trim);
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
